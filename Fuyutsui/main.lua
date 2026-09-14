7c66b8ca8d449ac7eec2de225e7b64312321a1bc
waynebian01 <bian.wayne@gmail.com>
2026-09-14 11:18:05 +0800

    feat: add RefreshAllPlayerPowers call in RefreshPlayerState function

--- Fuyutsui/main.lua
+++ Fuyutsui/main.lua
@@ -31,6 +31,7 @@ function Fuyutsui:RefreshPlayerState()
     self:RefreshBossUnitStates()
     self:RefreshEnemyCounts()
     self:RebuildGroupRoster()
+    self:RefreshAllPlayerPowers()
     C_Timer.After(1, function()
         self:RefreshPlayerConfigStateBlocks()
         self.isInitialized = true
